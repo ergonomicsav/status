b93ef63dad8acbff6f31cc6e4a02dee6a6d2a306
Vlad <ermac1988@mail.ru>
2019-10-14 18:32:45 +0300

    доработка 2

---
+++ app/Console/Commands/BotScannerDomains.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Domain;
+use Illuminate\Console\Command;
+use Telegram\Bot\Laravel\Facades\Telegram;
+
+class BotScannerDomains extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:botmonitoring';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sending a message to the bot telegram channel';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $arrDomains = Domain::where('status', '!=', [200,302])->get();
+        $arr = $arrDomains->sortByDesc('status')->pluck('status', 'domain');
+        $message = '<b>Мониторинг</b>' . PHP_EOL;
+        foreach ($arr as $domain => $status){
+            $message .= $domain . ' &gt; ' . '<b>' . $status . '</b>' . PHP_EOL;
+        }
+        $message = trim($message);
+//        dd($y);
+        Telegram::sendMessage([
+            'chat_id' => '-320333662',
+            'parse_mode' => 'HTML',
+            'text' => $message,
+        ]);
+    }
+}
