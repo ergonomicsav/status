a602ab3216b0933b252c01fd5a6608886654cdcd
Vlad <ermac1988@mail.ru>
2019-10-21 12:01:11 +0300

    edit script

--- config/file-manager.php
+++ config/file-manager.php
@@ -25,7 +25,7 @@ return [
      * List of disk names that you want to use
      * (from config/filesystems)
      */
-    'diskList' => ['logs'],
+    'diskList' => ['logs', 'public'],
 
     /**
      * Default disk for left manager
--- config/filesystems.php
+++ config/filesystems.php
@@ -62,11 +62,11 @@ return [
             'visibility' => 'public',
             'permissions' => [
                 'file' => [
-                    'public' => 0664,
+                    'public' => 0644,
                     'private' => 0600,
                 ],
                 'dir' => [
-                    'public' => 0775,
+                    'public' => 0755,
                     'private' => 0700,
                 ],
             ],
