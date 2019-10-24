909e56f88a06eeaacdb85fb26deb607c1f89c49c
Vlad <ermac1988@mail.ru>
2019-10-24 15:28:59 +0300

    добавлен новый класс DomainsScannerProcessing и изменен класс RunScannerDomens

--- app/Console/Commands/RunScannerDomens.php
+++ app/Console/Commands/RunScannerDomens.php
@@ -3,8 +3,9 @@
 namespace App\Console\Commands;
 
 use App\Models\Domain;
+use App\Repositories\DomainsScannerProcessing;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Cache;
+
 
 class RunScannerDomens extends Command
 {
@@ -22,6 +23,8 @@ class RunScannerDomens extends Command
      */
     protected $description = 'Domain scanning for monitoring purposes';
 
+    private $domainsScannerProcessing;
+
     /**
      * Create a new command instance.
      *
@@ -30,6 +33,7 @@ class RunScannerDomens extends Command
     public function __construct()
     {
         parent::__construct();
+        $this->domainsScannerProcessing = app(DomainsScannerProcessing::class);
     }
 
     /**
@@ -39,59 +43,61 @@ class RunScannerDomens extends Command
      */
     public function handle()
     {
-        $dms = Domain::pluck('domain', 'id')->all();
-        $mh = curl_multi_init();
-        foreach ($dms as $key => $value) {
-            $ch[$key] = curl_init($value);
-            curl_setopt($ch[$key], CURLOPT_NOBODY, true);
-            curl_setopt($ch[$key], CURLOPT_HEADER, true);
-            curl_setopt($ch[$key], CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch[$key], CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch[$key], CURLOPT_SSL_VERIFYHOST, false);
-//            curl_setopt($ch[$key], CURLOPT_FOLLOWLOCATION, true);
-
-            curl_multi_add_handle($mh, $ch[$key]);
-        }
-
-// Запустк
-        $running = null;
-        do {
-            curl_multi_exec($mh, $running);
-            curl_multi_select($mh);
-        } while ($running > 0);
-
-// Собираем массив для БД
-        foreach (array_keys($ch) as $key) {
-            $reportArray[] = [
-                'httpCode' => curl_getinfo($ch[$key], CURLINFO_HTTP_CODE),
-                'redirect_url' => curl_getinfo($ch[$key], CURLINFO_REDIRECT_URL),
-                'id' => $key,
-                'ip' => curl_getinfo($ch[$key], CURLINFO_PRIMARY_IP)
-            ];
-
-            curl_multi_remove_handle($mh, $ch[$key]);
-        }
-
-// Звкрываем curl
-        curl_multi_close($mh);
-// Запись в БД
-        $this->updToBd($reportArray);
-        Cache::put('new_time', date('H:i:s d/m/Y'));
+        $domains = Domain::all();
+        $this->domainsScannerProcessing->processing($domains);
+//        $dms = Domain::pluck('domain', 'id')->all();
+//        $mh = curl_multi_init();
+//        foreach ($dms as $key => $value) {
+//            $ch[$key] = curl_init($value);
+//            curl_setopt($ch[$key], CURLOPT_NOBODY, true);
+//            curl_setopt($ch[$key], CURLOPT_HEADER, true);
+//            curl_setopt($ch[$key], CURLOPT_RETURNTRANSFER, true);
+//            curl_setopt($ch[$key], CURLOPT_SSL_VERIFYPEER, false);
+//            curl_setopt($ch[$key], CURLOPT_SSL_VERIFYHOST, false);
+////            curl_setopt($ch[$key], CURLOPT_FOLLOWLOCATION, true);
+//
+//            curl_multi_add_handle($mh, $ch[$key]);
+//        }
+//
+//// Запустк
+//        $running = null;
+//        do {
+//            curl_multi_exec($mh, $running);
+//            curl_multi_select($mh);
+//        } while ($running > 0);
+//
+//// Собираем массив для БД
+//        foreach (array_keys($ch) as $key) {
+//            $reportArray[] = [
+//                'httpCode' => curl_getinfo($ch[$key], CURLINFO_HTTP_CODE),
+//                'redirect_url' => curl_getinfo($ch[$key], CURLINFO_REDIRECT_URL),
+//                'id' => $key,
+//                'ip' => curl_getinfo($ch[$key], CURLINFO_PRIMARY_IP)
+//            ];
+//
+//            curl_multi_remove_handle($mh, $ch[$key]);
+//        }
+//
+//// Звкрываем curl
+//        curl_multi_close($mh);
+//// Запись в БД
+//        $this->updToBd($reportArray);
+//        Cache::put('new_time', date('H:i:s d/m/Y'));
 //        Cache::put('new_time', date('H:i:s d/m/Y'), now()->addMinutes(1));
 //        return redirect()->route('domains.index');
 
     }
 
-    private function updToBd($items)
-    {
-        if ($items) {
-            foreach ($items as $var) {
-                $domains = Domain::find($var['id']);
-                $domains->ip = $var['ip'];
-                $domains->redirect_url = $var['redirect_url'] ? $var['redirect_url'] : 'нет';
-                $domains->status = $var['httpCode'];
-                $domains->save();
-            }
-        }
-    }
+//    private function updToBd($items)
+//    {
+//        if ($items) {
+//            foreach ($items as $var) {
+//                $domains = Domain::find($var['id']);
+//                $domains->ip = $var['ip'];
+//                $domains->redirect_url = $var['redirect_url'] ? $var['redirect_url'] : 'нет';
+//                $domains->status = $var['httpCode'];
+//                $domains->save();
+//            }
+//        }
+//    }
 }
---
+++ app/Repositories/DomainsScannerProcessing.php
@@ -0,0 +1,75 @@
+<?php
+
+
+namespace App\Repositories;
+
+
+use App\Models\Domain as Model;
+use Illuminate\Support\Facades\Cache;
+
+class DomainsScannerProcessing extends CoreRepository
+{
+    protected function getModelClass()
+    {
+        return Model::class;
+    }
+
+    public function processing($domainss)
+    {
+        $dms = $domainss->pluck('domain', 'id');
+        $mh = curl_multi_init();
+        foreach ($dms as $key => $value) {
+            $ch[$key] = curl_init($value);
+            curl_setopt($ch[$key], CURLOPT_NOBODY, true);
+            curl_setopt($ch[$key], CURLOPT_HEADER, true);
+            curl_setopt($ch[$key], CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch[$key], CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch[$key], CURLOPT_SSL_VERIFYHOST, false);
+//            curl_setopt($ch[$key], CURLOPT_FOLLOWLOCATION, true);
+
+            curl_multi_add_handle($mh, $ch[$key]);
+        }
+
+// Запустк
+        $running = null;
+        do {
+            curl_multi_exec($mh, $running);
+            curl_multi_select($mh);
+        } while ($running > 0);
+
+// Собираем массив для БД
+        foreach (array_keys($ch) as $key) {
+            $reportArray[] = [
+                'httpCode' => curl_getinfo($ch[$key], CURLINFO_HTTP_CODE),
+                'redirect_url' => curl_getinfo($ch[$key], CURLINFO_REDIRECT_URL),
+                'id' => $key,
+                'ip' => curl_getinfo($ch[$key], CURLINFO_PRIMARY_IP)
+            ];
+
+            curl_multi_remove_handle($mh, $ch[$key]);
+        }
+
+// Звкрываем curl
+        curl_multi_close($mh);
+// Запись в БД
+        $this->updToBd($reportArray);
+        Cache::put('new_time', date('H:i:s d/m/Y'));
+//        Cache::put('new_time', date('H:i:s d/m/Y'), now()->addMinutes(1));
+//        return redirect()->route('domains.index');
+
+    }
+
+    private function updToBd($items)
+    {
+        if ($items) {
+            foreach ($items as $var) {
+                $domains = Model::find($var['id']);
+                $domains->ip = $var['ip'];
+                $domains->redirect_url = $var['redirect_url'] ? $var['redirect_url'] : 'нет';
+                $domains->status = $var['httpCode'];
+                $domains->save();
+            }
+        }
+    }
+
+}
